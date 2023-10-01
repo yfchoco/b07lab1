86e1416d12b6e5ccfe4438005daf8f3e61d3ce88
福原悠平 <yfchoco@fukuharuheisair.home>
2023-10-01 12:47:06 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,18 +1,52 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial(); 
 		System.out.println(p.evaluate(3));
-		double [] c1 = {1};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2); 
+		double [] c1 = {2,-8,10,3};
+		int [] e1 = {2,1,0,3};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double [] c2 = {-1,2,-1,-3};
+		int [] e2 = {2,1,0,3};
+		Polynomial p2 = new Polynomial(c2, e2); 
 		Polynomial s = p1.add(p2); 
+		Polynomial prod = p1.multiply(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1)) {
-			System.out.println("1 is a root of s");
+		System.out.println("prod(-2.37775) = " + prod.evaluate(-2.37775));
+		System.out.println("length of prod is " + prod.exponents.length);
+		if(s.hasRoot(3)) {
+			System.out.println("3 is a root of s");
 		}
 		else{
-			System.out.println("1 is not a root of s");
+			System.out.println("3 is not a root of s");
+		}
+
+		if(prod.hasRoot(3)) {
+			System.out.println("3 is a root of prod");
+		}
+		else{
+			System.out.println("3 is not a root of prod");
+		}
+
+		File file = new File("polynomial.txt");
+		Polynomial p4 = new Polynomial(file);
+		System.out.println("p4(5) = " + p4.evaluate(6));
+
+		try {
+    		prod.saveToFile("prod.txt");
+		} 
+		catch (IOException e) {
+    	// Handle the exception, e.g., print an error message
+    		e.printStackTrace();
+		}
+		try {
+    		p4.saveToFile("p4.txt");
+		} 
+		catch (IOException e) {
+    	// Handle the exception, e.g., print an error message
+    		e.printStackTrace();
 		}
 	} 
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,35 +1,165 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class Polynomial {
-	double [] coeffs;
+	double[] coeffs;
+	int[] exponents;
 
 	public Polynomial() {
-		double [] zeros = {0};
-		coeffs = zeros;
+		double[] zerosDouble = {0};
+		int[] zerosInt = {0};
+		coeffs = zerosDouble;
+		exponents = zerosInt;
 	}
 
-	public Polynomial(double [] c) {
+	public Polynomial(double[] c, int[] e) {
 		coeffs = c;
+		exponents = e;
+	}
+
+	public Polynomial(File f) {
+		char[] polArr = new char[100];
+		String polStr = "";
+
+		try {
+			Scanner myReader = new Scanner(f);
+			while (myReader.hasNextLine()) {
+				polStr = myReader.nextLine();
+			}
+     		myReader.close();
+
+     		String[] polStrSpl = polStr.split("(?=-|\\+)");
+			double[] c = new double[polStrSpl.length];
+			int[] e = new int[polStrSpl.length];
+
+			for (int i = 0; i < polStrSpl.length; i++) {
+				String[] coeffAndExp = polStrSpl[i].split("x");
+
+				if (coeffAndExp.length == 2) {
+					c[i] = Double.parseDouble(coeffAndExp[0]);
+					e[i] = Integer.parseInt(coeffAndExp[1]);
+				}
+				else {
+					c[i] = Double.parseDouble(coeffAndExp[0]);
+					e[i] = 0;
+				}
+			}
+			coeffs = c;
+			exponents = e;
+    	} 
+    	catch (FileNotFoundException er) {
+      		System.out.println("File error");
+      		er.printStackTrace();
+    	}
+	}
+
+
+	public boolean inExponents(int exp) {
+		// return true if exp is in this.exponents
+		for (int i = 0; i < this.exponents.length; i++) {
+			if (this.exponents[i] == exp) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void addExp(Polynomial pol, List<Integer> list) {
+		// combine the exponents in this and pol and add to list
+		for (int i = 0; i < pol.exponents.length; i++) {
+			if (! this.inExponents(pol.exponents[i])){
+				// add if only in pol.exponents
+				list.add(pol.exponents[i]);
+			}
+		}
+		for (int i = 0; i < this.exponents.length; i++) {
+			// add everything in this.exponents
+			list.add(this.exponents[i]);
+		}
+	}
+
+	public double coeffFromExp(int exp) {
+		// return the coef that corresponds to exp or 0 if dne
+		for (int i = 0; i < this.exponents.length; i++) {
+			if (this.exponents[i] == exp) {
+				return this.coeffs[i];
+			}
+		}
+		return 0;
 	}
 
 	public Polynomial add(Polynomial pol) {
-		if (this.coeffs.length > pol.coeffs.length) {
-			for (int i = 0; i < pol.coeffs.length; i++) {
-				this.coeffs[i] += pol.coeffs[i];
+		List<Integer> expList = new ArrayList<>();
+		this.addExp(pol, expList);
+		int[] expArray = new int [expList.size()];
+		double[] coeffArray = new double [expArray.length];
+
+		// convert list into array and add coeffs to corresponding exponent
+        for (int i = 0; i < expList.size(); i++) {
+            expArray[i] = expList.get(i);
+            coeffArray[i] += this.coeffFromExp(expArray[i]);
+			coeffArray[i] += pol.coeffFromExp(expArray[i]);
+        }
+
+		Polynomial sumPol = new Polynomial(coeffArray, expArray);
+		return sumPol;
+	}
+
+	public int findExp(List<Integer> list, int exp) {
+		// return the index of exp if it is in list and -1 otherwise
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == exp) {
+				return i;
 			}
-			return this;
 		}
-		else {
-			for (int i = 0; i < this.coeffs.length; i++) {
-				pol.coeffs[i] += this.coeffs[i];
+		return -1;
+	}
+
+	public Polynomial multiply(Polynomial pol) {
+		List<Double> coeffList = new ArrayList<>();
+		List<Integer> expList = new ArrayList<>();
+		double coeffProd = 0;
+		int expProd = 0;
+		int expFound = 0;
+
+		for (int i = 0; i < this.exponents.length; i++) {
+			for (int j = 0; j < pol.exponents.length; j++) {
+				coeffProd = this.coeffs[i] * pol.coeffs[j];
+				expProd = this.exponents[i] + pol.exponents[j];
+				expFound = findExp(expList, expProd);
+				if (expFound == -1) {
+					coeffList.add(coeffProd);
+					expList.add(expProd);
+				}
+				else {
+					coeffProd += coeffList.get(expFound);
+					coeffList.set(expFound, coeffProd);
+				}
 			}
-			return pol;
 		}
+
+		int[] expArray = new int [expList.size()];
+		double[] coeffArray = new double [expArray.length];
+
+		for (int i = 0; i < expList.size(); i++) {
+            expArray[i] = expList.get(i);
+            coeffArray[i] = coeffList.get(i);
+        }
+        Polynomial prodPol = new Polynomial(coeffArray, expArray);
+		return prodPol;
 	}
 
 	public double evaluate(double x) {
 		double total = 0;
 		for (int i = 0; i < this.coeffs.length; i++) {
-			double part_sum = x;
-			for (int j = 0; j < i; j++) {
+			double part_sum = 1;
+			for (int j = 0; j < this.exponents[i]; j++) {
 				part_sum *= x;
 			}
 			total += this.coeffs[i] * part_sum;
@@ -39,5 +169,29 @@ public class Polynomial {
 
 	public boolean hasRoot(double x) {
 		return this.evaluate(x) == 0;
+	}
+
+	public void saveToFile(String fName) throws IOException {
+		String polStr = "";
+		for (int i = 0; i < this.exponents.length; i++) {
+			if (this.exponents[i] != 0) {
+				if ((this.coeffs[i] > 0) && (i != 0)) {
+					polStr += "+";
+				}
+				polStr += String.format("%.1fx%d", this.coeffs[i], this.exponents[i]);
+			}
+			else {
+				if ((i != 0) && (this.coeffs[i] > 0)) {
+					polStr += "+";
+				}
+				polStr += String.format("%.1f", this.coeffs[i]);
+			}
+		}
+
+		File file = new File(fName);
+		FileWriter wr = new FileWriter(file);
+		wr.write(polStr);
+		wr.flush();
+		wr.close();
 	}
 }
